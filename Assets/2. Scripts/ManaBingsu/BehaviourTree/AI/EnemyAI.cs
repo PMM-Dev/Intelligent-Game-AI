a3703ad2ead956757ae67ba848e4080f6f0c3fb9
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-11-04 14:48:07 +0900

    feat : Health node, EnemyAI

---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/AI/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaBingsu
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        [SerializeField] 
+        private float _startingHealth;
+        [SerializeField] 
+        private float _lowHealthThreshold;
+        [SerializeField] 
+        private float _healthRestoreRate;
+
+        [SerializeField] 
+        private float _currentHealth;
+        public float CurrentHealth
+        {
+            get { return _currentHealth; }
+            set { _currentHealth = Mathf.Clamp(value, 0, _startingHealth); }
+        }
+
+        private void Start()
+        {
+            _currentHealth = _startingHealth;
+        }
+
+        public void Update()
+        {
+            CurrentHealth += Time.deltaTime * _healthRestoreRate;
+        }
+    }
+}
+
---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Nodes/HealthNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaBingsu
+{
+    public class HealthNode : Node
+    {
+        private EnemyAI _ai;
+        private float _threshold;
+
+        public HealthNode(EnemyAI ai, float threshold)
+        {
+            _ai = ai;
+            _threshold = threshold;
+        }
+
+        public override NodeState Evaluate()
+        {
+            return _ai.CurrentHealth <= _threshold ? NodeState.Success : NodeState.Failure;
+        }
+    }
+}
