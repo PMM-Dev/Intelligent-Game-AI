3ee9284b32c3fe2d2d3c816c975d88e039f5b510
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-11-04 13:29:11 +0900

    feat : Node for behaviour tree

---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+namespace ManaBingsu
+{
+    [System.Serializable]
+    public abstract class Node
+    {
+        protected NodeState _nodeState;
+        public NodeState nodeState { get => _nodeState; }
+
+        public abstract NodeState Evaluate();
+    }
+
+    public enum NodeState
+    {
+        Running,
+        Success,
+        Failure,
+    }
+}
+
+
