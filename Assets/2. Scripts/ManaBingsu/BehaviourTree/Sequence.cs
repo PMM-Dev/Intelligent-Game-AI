61295d7b715500a30fd48f72b1075a7c96c7fb51
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-11-04 13:35:45 +0900

    feat : Sequence for behaviour tree

---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Sequence.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaBingsu
+{
+    public class Sequence : Node
+    {
+        public List<Node> nodes = new List<Node>();
+
+        public Sequence(List<Node> nodes)
+        {
+            this.nodes = nodes;
+        }
+
+        public override NodeState Evaluate()
+        {
+            bool isAnyNodeRunning = false;
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                switch(nodes[i].Evaluate())
+                {
+                    case NodeState.Running:
+                        isAnyNodeRunning = true;
+                        break;
+                    case NodeState.Success:
+                        break;
+                    case NodeState.Failure:
+                        _nodeState = NodeState.Failure;
+                        return _nodeState;
+                    default:
+                        break;
+                }
+            }
+
+            _nodeState = isAnyNodeRunning ? NodeState.Running : NodeState.Success;
+            return _nodeState;
+        }
+    }
+}
