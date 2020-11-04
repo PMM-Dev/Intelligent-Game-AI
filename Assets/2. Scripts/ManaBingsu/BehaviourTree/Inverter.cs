c5e64f4a8bc3e0d18f0726a48fe28bd30811503b
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-11-04 14:33:47 +0900

    feat : Selector node, Inverter node

---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Inverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaBingsu
+{
+    public class Inverter : Node
+    {
+        public Node node;
+
+        public Inverter(Node node)
+        {
+            this.node = node;
+        }
+
+        public override NodeState Evaluate()
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.Running:
+                    _nodeState = NodeState.Running;
+                    break;
+                case NodeState.Success:
+                    _nodeState = NodeState.Failure;
+                    break;
+                case NodeState.Failure:
+                    _nodeState = NodeState.Success;
+                    return _nodeState;
+                default:
+                    break;
+            }
+            return _nodeState;
+        }
+    }
+}
---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Selector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManaBingsu
+{
+    public class Selector : Node
+    {
+        public List<Node> nodes = new List<Node>();
+
+        public Selector(List<Node> nodes)
+        {
+            this.nodes = nodes;
+        }
+
+        public override NodeState Evaluate()
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                switch(nodes[i].Evaluate())
+                {
+                    case NodeState.Running:
+                        _nodeState = NodeState.Running;
+                        return _nodeState;
+                    case NodeState.Success:
+                        _nodeState = NodeState.Success;
+                        return _nodeState;
+                    case NodeState.Failure:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            _nodeState = NodeState.Failure;
+            return _nodeState;
+        }
+    }
+}
