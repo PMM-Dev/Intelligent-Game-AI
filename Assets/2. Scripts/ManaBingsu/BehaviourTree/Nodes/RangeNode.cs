7ba973e5a3f6ac2093b5e591d04202479154a928
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-11-04 14:53:49 +0900

    feat : Range node

---
+++ Assets/2. Scripts/ManaBingsu/BehaviourTree/Nodes/RangeNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+namespace ManaBingsu
+{
+    public class RangeNode : Node
+    {
+        private float _range;
+        private Transform _target;
+        private Transform _origin;
+
+        public RangeNode(float range, Transform target, Transform origin)
+        {
+            _range = range;
+            _target = target;
+            _origin = origin;
+        }
+
+        public override NodeState Evaluate()
+        {
+            float distance = Vector3.Distance(_target.position, _origin.position);
+            return distance <= _range ? NodeState.Success : NodeState.Failure;
+        }
+    }
+
+}
